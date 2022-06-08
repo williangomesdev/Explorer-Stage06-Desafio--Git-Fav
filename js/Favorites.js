67a3e636df94d2800f15ae15b411acb258ebfe43
User <willianamaroti@gmail.com>
2022-06-08 14:36:58 -0300

    adicionando funções de atualizar e remover linhas da tabela

--- js/Favorites.js
+++ js/Favorites.js
@@ -11,6 +11,17 @@ export class Favorites {
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root);
-    console.log(this.root);
+    this.update();
+  }
+
+  update() {
+    this.removeAllTr();
+  }
+
+  removeAllTr() {
+    const tbody = this.root.querySelector("table tbody");
+    tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
   }
 }
