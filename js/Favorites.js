44af37196ddffbed43737f5595cd4bc967379cc7
User <willianamaroti@gmail.com>
2022-06-08 14:43:11 -0300

    adicionando conteudo html no javascript para ser criado

--- js/Favorites.js
+++ js/Favorites.js
@@ -18,6 +18,23 @@ export class FavoritesView extends Favorites {
     this.removeAllTr();
   }
 
+  createRow() {
+    const content = `
+    
+        <td class="users">
+            <img src="https://github.com/williangomesdev.png" alt="imagem github profile"/>
+            <a href="https://github.com/williangomesdev" target="_blank">
+                <p>Willian Amaro Gomes</p>
+                <span>/williangomesdev</span>
+            </a>
+        </td>
+        <td class="repositories">100</td>
+        <td class="followers">1000</td>
+        <td class="action"><button>Remover</button></td>
+    
+    `;
+  }
+
   removeAllTr() {
     const tbody = this.root.querySelector("table tbody");
     tbody.querySelectorAll("tr").forEach((tr) => {
